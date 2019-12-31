9086240d370fb054af6028ba8dc51bdb3ff93bbd
(iYaReM) Daniel Jacob <danieljacobowp@gmail.com>
2019-12-31 02:41:04 -0300

    Add files via upload

---
+++ Excess Nop Remover/Program.cs
@@ -0,0 +1,68 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Writer;
+using System;
+using System.IO;
+
+namespace Excess_Nop_Remover
+{
+    class Program
+    {
+        public static string Asmpath;
+        public static ModuleDefMD AsmethodMdOriginal;
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Excess Nop Remover - iYaReM";
+
+            if (args.Length == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Drag'n drop file.");
+                Console.ReadKey();
+                return;
+            }
+
+            try
+            {
+                AsmethodMdOriginal = ModuleDefMD.Load(args[0]);
+                Asmpath = args[0];
+
+                string directoryName = Path.GetDirectoryName(args[0]);
+
+                if (!directoryName.EndsWith("\\"))
+                {
+                    directoryName += "\\";
+                }
+
+                Remover.NopRemover(AsmethodMdOriginal);
+                Console.WriteLine($"Working..");
+
+                string filename = string.Format("{0}{1}-NopRemoved{2}", directoryName, Path.GetFileNameWithoutExtension(args[0]), Path.GetExtension(args[0]));
+
+                if (!AsmethodMdOriginal.Is32BitRequired)
+                {
+                    ModuleWriterOptions moduleWriterOptions = new ModuleWriterOptions(AsmethodMdOriginal);
+                    moduleWriterOptions.MetaDataLogger = DummyLogger.NoThrowInstance;
+                    moduleWriterOptions.MetaDataOptions.Flags = MetaDataFlags.PreserveAll;
+                    AsmethodMdOriginal.Write(filename, moduleWriterOptions);
+                }
+                else
+                {
+                    NativeModuleWriterOptions moduleWriterOptions = new NativeModuleWriterOptions(AsmethodMdOriginal);
+                    moduleWriterOptions.MetaDataLogger = DummyLogger.NoThrowInstance;
+                    moduleWriterOptions.MetaDataOptions.Flags = MetaDataFlags.PreserveAll;
+                    AsmethodMdOriginal.NativeWrite(filename, moduleWriterOptions);
+                }
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("");
+                Console.WriteLine("Done! Saving Assembly...");
+            }
+            catch 
+            { 
+                Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Invalid file."); 
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Excess Nop Remover/Remover.cs
@@ -0,0 +1,127 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+
+namespace Excess_Nop_Remover
+{
+    internal class Remover
+    {
+        public static void NopRemover(ModuleDefMD modulee)
+        {
+            foreach (TypeDef typeDef in modulee.Types)
+            {
+                foreach (MethodDef methodDef in typeDef.Methods)
+                {
+                    if (methodDef.HasBody)
+                    {
+                        RemoveUnusedNops(methodDef);
+                    }
+                }
+            }
+        }
+
+        public static void RemoveUnusedNops(MethodDef MethodDef)
+        {
+            if (MethodDef.HasBody)
+            {
+                for (int i = 0; i < MethodDef.Body.Instructions.Count; i++)
+                {
+                    Instruction instruction = MethodDef.Body.Instructions[i];
+                    if (instruction.OpCode == OpCodes.Nop)
+                    {
+                        if (!IsNopBranchTarget(MethodDef, instruction))
+                        {
+                            if (!IsNopSwitchTarget(MethodDef, instruction))
+                            {
+                                if (!IsNopExceptionHandlerTarget(MethodDef, instruction))
+                                {
+                                    MethodDef.Body.Instructions.RemoveAt(i);
+                                    i--;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static bool IsNopBranchTarget(MethodDef MethodDef, Instruction NopInstr)
+        {
+            for (int i = 0; i < MethodDef.Body.Instructions.Count; i++)
+            {
+                Instruction instruction = MethodDef.Body.Instructions[i];
+                if (instruction.OpCode.OperandType == OperandType.InlineBrTarget || instruction.OpCode.OperandType == OperandType.ShortInlineBrTarget)
+                {
+                    if (instruction.Operand != null)
+                    {
+                        Instruction instruction2 = (Instruction)instruction.Operand;
+                        if (instruction2 == NopInstr)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        private static bool IsNopSwitchTarget(MethodDef MethodDef, Instruction NopInstr)
+        {
+            for (int i = 0; i < MethodDef.Body.Instructions.Count; i++)
+            {
+                Instruction instruction = MethodDef.Body.Instructions[i];
+                if (instruction.OpCode.OperandType == OperandType.InlineSwitch)
+                {
+                    if (instruction.Operand != null)
+                    {
+                        Instruction[] source = (Instruction[])instruction.Operand;
+                        if (source.Contains(NopInstr))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        private static bool IsNopExceptionHandlerTarget(MethodDef MethodDef, Instruction NopInstr)
+        {
+            bool result;
+            if (!MethodDef.Body.HasExceptionHandlers)
+            {
+                result = false;
+            }
+            else
+            {
+                IList<ExceptionHandler> exceptionHandlers = MethodDef.Body.ExceptionHandlers;
+                foreach (ExceptionHandler exceptionHandler in exceptionHandlers)
+                {
+                    if (exceptionHandler.FilterStart == NopInstr)
+                    {
+                        return true;
+                    }
+                    if (exceptionHandler.HandlerEnd == NopInstr)
+                    {
+                        return true;
+                    }
+                    if (exceptionHandler.HandlerStart == NopInstr)
+                    {
+                        return true;
+                    }
+                    if (exceptionHandler.TryEnd == NopInstr)
+                    {
+                        return true;
+                    }
+                    if (exceptionHandler.TryStart == NopInstr)
+                    {
+                        return true;
+                    }
+                }
+                result = false;
+            }
+            return result;
+        }
+    }
+}
