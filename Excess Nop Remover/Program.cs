b75e41788b9e365b2897e2f686a9f906baeca7bd
Daniel Jacob <danieljacobowp@gmail.com>
2022-03-12 03:47:31 -0300

    Update Program.cs

--- Excess Nop Remover/Program.cs
+++ Excess Nop Remover/Program.cs
@@ -1,14 +1,14 @@
-﻿using dnlib.DotNet;
-using dnlib.DotNet.Writer;
 using System;
 using System.IO;
+using dnlib.DotNet;
+using dnlib.DotNet.Writer;
 
 namespace Excess_Nop_Remover
 {
     class Program
     {
-        public static string Asmpath;
-        public static ModuleDefMD AsmethodMdOriginal;
+        private static string Asmpath;
+        private static ModuleDefMD AsmethodMdOriginal;
 
         static void Main(string[] args)
         {
@@ -27,41 +27,47 @@ namespace Excess_Nop_Remover
                 AsmethodMdOriginal = ModuleDefMD.Load(args[0]);
                 Asmpath = args[0];
 
-                string directoryName = Path.GetDirectoryName(args[0]);
+                var directoryName = Path.GetDirectoryName(args[0]);
 
                 if (!directoryName.EndsWith("\\"))
-                {
                     directoryName += "\\";
-                }
 
                 Remover.NopRemover(AsmethodMdOriginal);
-                Console.WriteLine($"Working..");
+                Console.WriteLine("Working..");
 
-                string filename = string.Format("{0}{1}-NopRemoved{2}", directoryName, Path.GetFileNameWithoutExtension(args[0]), Path.GetExtension(args[0]));
+                var filename = $"{directoryName}{Path.GetFileNameWithoutExtension(Asmpath)}-NopRemoved{Path.GetExtension(Asmpath)}";
 
                 if (!AsmethodMdOriginal.Is32BitRequired)
                 {
-                    ModuleWriterOptions moduleWriterOptions = new ModuleWriterOptions(AsmethodMdOriginal);
-                    moduleWriterOptions.MetaDataLogger = DummyLogger.NoThrowInstance;
-                    moduleWriterOptions.MetaDataOptions.Flags = MetaDataFlags.PreserveAll;
+                    var moduleWriterOptions = new ModuleWriterOptions(AsmethodMdOriginal)
+                    {
+                        MetaDataLogger = DummyLogger.NoThrowInstance,
+                        MetaDataOptions =
+                        {
+                            Flags = MetaDataFlags.PreserveAll
+                        }
+                    };
                     AsmethodMdOriginal.Write(filename, moduleWriterOptions);
                 }
                 else
                 {
-                    NativeModuleWriterOptions moduleWriterOptions = new NativeModuleWriterOptions(AsmethodMdOriginal);
-                    moduleWriterOptions.MetaDataLogger = DummyLogger.NoThrowInstance;
-                    moduleWriterOptions.MetaDataOptions.Flags = MetaDataFlags.PreserveAll;
+                    var moduleWriterOptions = new NativeModuleWriterOptions(AsmethodMdOriginal)
+                    {
+                        MetaDataLogger = DummyLogger.NoThrowInstance,
+                        MetaDataOptions =
+                        {
+                            Flags = MetaDataFlags.PreserveAll
+                        }
+                    };
                     AsmethodMdOriginal.NativeWrite(filename, moduleWriterOptions);
                 }
 
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("");
-                Console.WriteLine("Done! Saving Assembly...");
+
+                Console.WriteLine("\nDone! Saving Assembly...");
             }
-            catch 
-            { 
-                Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Invalid file."); 
-            }
+            catch { Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Invalid file."); }
+
             Console.ReadKey();
         }
     }
